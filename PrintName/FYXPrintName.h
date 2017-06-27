0cc86d1d697e538c739fac512fe37a51dffa7876
梧桐 <1536262494@qq.com>
2017-06-27 23:50:48 +0800

    add PrintName

---
+++ PrintName/FYXPrintName.h
@@ -0,0 +1,13 @@
+//
+//  FYXPrintName.h
+//  mergeTest
+//
+//  Created by 范逸潇 on 2017/6/27.
+//  Copyright © 2017年 范逸潇. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FYXPrintName : NSObject
++(void)printOurName;
+@end
---
+++ PrintName/FYXPrintName.m
@@ -0,0 +1,15 @@
+//
+//  FYXPrintName.m
+//  mergeTest
+//
+//  Created by 范逸潇 on 2017/6/27.
+//  Copyright © 2017年 范逸潇. All rights reserved.
+//
+
+#import "FYXPrintName.h"
+
+@implementation FYXPrintName
++(void)printOurName{
+    NSLog(@"Hello world!");
+}
+@end
